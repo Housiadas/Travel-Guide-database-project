cc2a314dd505db2ad631015e4bc8aae9dfbe233c
Χρηστος Χουσιαδας <chousiadas6395@gmail.com>
2019-12-28 00:10:14 +0200

    db and gui v.2

--- codes/DatabaseGui.py
+++ codes/DatabaseGui.py
@@ -1,12 +1,12 @@
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import messagebox
+from tkinter import Grid
+from tkinter import ttk
 import TravelGuide as tg
+import json
 
-### db instance
-db = tg.Database()
-
-def add_item():
-    print(f'Hellow')
+### db instance .....
+db = tg.Database('travel_guide')
 
 def populateListSights(event):
     commandList.delete(0, tk.END)
@@ -28,68 +28,115 @@ def populateListHotels(event):
 def populateListTouristPackets (event):
     commandList.delete(0, tk.END)
     for row in db.PrintTouristPackets():
-        commandList.insert(tk.END, row)
+         commandList.insert(tk.END, row)
 
 def populateListTransports(event):
     commandList.delete(0, tk.END)
     for row in db.PrintTransports():
         commandList.insert(tk.END, row)
 
-def clearText():
-    print(f'clear')
+def sqlCommand(event):
+    commandList.delete(0, tk.END)
+    if(commandText.get() ==""):
+        messagebox.showerror("Required Field", "Please insert command!")
+        return
+    else:
+        response = db.GiveSqlCommand(commandText.get())
+        response = json.loads(response)
+        if(response["error"] != ""):
+            print(response["error"])
+            messagebox.showerror("Command Error", response["error"])
+        else:
+            print(response["res"])
+            commandList.insert(tk.END, response["res"])
 
-# tg.DBConnection();
+def selectItem(event):
+    global selectedItem
+    index = commandList.curselection()[0]
+    selectedItem = commandList.get(index)
+    print(selectedItem)
 
-### create window object
+#################################################################################################################
+######### create window object
 app = tk.Tk()
+
 app.title("Travel Guide DB system")
-app.geometry("700x450")
+app.geometry("720x500")
 app.configure(background='grey25')
 app.iconbitmap('favicon.ico')
 
-### command
-commandText = tk.StringVar()
-commandLabel = tk.Label(app, text="SQL command", font=("bold", 14),
-    pady=14, padx=14, background='grey25', fg="white")
-commandLabel.grid(row=0, column=0, sticky=tk.W)
-commandEntry = tk.Entry(app, textvariable=commandText, width=50)
-commandEntry.grid(row=0, column=1)
+##### responsive grid
+Grid.rowconfigure(app, 0, weight=1)
+Grid.columnconfigure(app, 0, weight=1)
 
-### list box (listbox)
-commandList = tk.Listbox(app, height=15, width=90, border=0)
-commandList.grid(row=3, column=0, columnspan=3, rowspan=6, pady=14)
+frame1 = tk.Frame(app, background="grey25")
+frame1.grid(row=0, column=0, sticky=tk.N)
+
+frame2 = tk.Frame(app, background="grey25")
+frame2.grid(row=1, column=0, sticky=tk.S+tk.N+tk.E+tk.W)
+Grid.rowconfigure(frame2, 0, weight=1)
+Grid.columnconfigure(frame2, 0, weight=1)
+
+### command Text....
+commandText = tk.StringVar()
+commandLabel = tk.Label(frame1, text="SQL command", font=("bold", 16),
+    pady=14, padx=14, background='grey25', fg="white")
+commandLabel.grid(row=0, column=1)
+commandEntry = tk.Entry(frame1, textvariable=commandText, width=50)
+commandEntry.grid(row=0, column=2)
+
+### Listbox (listbox)
+commandList = tk.Listbox(frame2, height=15, width=100, border=0)
+commandList.grid(row=0, column=0, columnspan=3, rowspan=6, pady=14, padx=14, sticky=tk.S+tk.N+tk.E+tk.W)
+commandList.bind("<<ListboxSelect>>", selectItem)
 
 ### create scroll bar...
-scrollbar = tk.Scrollbar(app)
-scrollbar.grid(row=3, column=2)
+scrollbar = tk.Scrollbar(frame2)
+scrollbar.grid(row=0, column=2, sticky=tk.N)
 commandList.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=commandList.yview)
 
 ### Buttons....
-addBtn1 = tk.Button(app, text="Execute SQL command", width=20, command=add_item)
-addBtn1.grid(row=1, column=0, pady=14, padx=14)
+sqlBtn = tk.Button(frame1, text="Execute SQL command", width=20)
+sqlBtn.bind("<Button-1>", sqlCommand)
+sqlBtn.grid(row=1, column=1, pady=14, padx=14)
 
-sightsBtn = tk.Button(app, text="Show Sights", width=20)
+sightsBtn = tk.Button(frame1, text="Show Sights", width=20)
 sightsBtn.bind("<Button-1>", populateListSights)
-sightsBtn.grid(row=1, column=1, pady=14, padx=14)
+sightsBtn.grid(row=1, column=2, pady=14, padx=14)
 
-hotelsBtn = tk.Button(app, text="Show Hotels", width=20)
+hotelsBtn = tk.Button(frame1, text="Show Hotels", width=20)
 hotelsBtn.bind("<Button-1>", populateListHotels)
-hotelsBtn.grid(row=1, column=2, pady=14, padx=14)
+hotelsBtn.grid(row=1, column=3, pady=14, padx=14)
 
-foodBtns = tk.Button(app, text="Show Restaurants", width=20)
-foodBtns.bind("<Button-1>", populateListRestaurants)
-foodBtns.grid(row=2, column=0, pady=14, padx=14)
+foodBtn = tk.Button(frame1, text="Show Restaurants", width=20)
+foodBtn.bind("<Button-1>", populateListRestaurants)
+foodBtn.grid(row=2, column=1, pady=14, padx=14)
 
-transpBtn = tk.Button(app, text="Show Transportation", width=20)
+transpBtn = tk.Button(frame1, text="Show Transportation", width=20)
 transpBtn.bind("<Button-1>", populateListTransports)
-transpBtn.grid(row=2, column=1, pady=14, padx=14)
+transpBtn.grid(row=2, column=2, pady=14, padx=14)
 
-packetsBtn = tk.Button(app, text="Show Packets", width=20, command=clearText)
+packetsBtn = tk.Button(frame1, text="Show Packets", width=20)
 packetsBtn.bind("<Button-1>", populateListTouristPackets)
-packetsBtn.grid(row=2, column=2, pady=14, padx=14)
+packetsBtn.grid(row=2, column=3, pady=14, padx=14)
+
+packetsBtn = tk.Button(frame1, text="Delete Item", width=20)
+packetsBtn.bind("<Button-1>", populateListTouristPackets)
+packetsBtn.grid(row=3, column=2, pady=14, padx=14)
+
+#### line seperator
+ttk.Separator(frame1, orient=tk.VERTICAL).grid(column=0, row=0, rowspan=4, sticky='ns')
+ttk.Separator(frame1, orient=tk.VERTICAL).grid(column=4, row=0, rowspan=4, sticky='ns')
+ttk.Separator(frame1, orient=tk.HORIZONTAL).grid(row=4, columnspan=4, sticky="ew")
 
 
-### start program
+#### responsive grid
+for x in range(0):
+  Grid.columnconfigure(frame2, x, weight=1)
+for y in range(1):
+  Grid.rowconfigure(frame2, y, weight=1)
+
+#### start program
 app.mainloop()
 
--- codes/TravelGuide.py
+++ codes/TravelGuide.py
@@ -1,17 +1,17 @@
 import pymysql
-from prettytable import PrettyTable
-from prettytable import from_db_cursor
+import json
+# from prettytable import PrettyTable
+# from prettytable import from_db_cursor
 
 class Database:
 
-    def __init__(self):
+    def __init__(self, db):
 
         host = 'localhost'
 
         try:
-            dbname ='travel_guide'
             self.con = pymysql.connect(host = host, port=3306,
-                 user='root', passwd=None, db=dbname, charset='utf8')
+                 user='root', passwd=None, db=db, charset='utf8')
         except pymysql.Error as e:
             print ('Σφάλμα', e)
         else:
@@ -22,55 +22,45 @@ class Database:
             print('Kάλωσήρθατε στην βάση δεδομένων του ταξιδιώτικου οδηγού')
 
 
-    def GiveSqlCommand (self):
-        # print('Δώσε την εντολή mysql (enter για έξοδο)')
-        buffer = ""
-        while True:
-            line = input('>>>')
-            if line == "":
-                print ("bye")
-                break
-            buffer += line
-            print (buffer)
-            if True:
-                try:
-                    buffer = buffer.strip()
-                    if buffer.lstrip().upper().startswith("SELECT"):
-                        count=0
-                        cur.execute(buffer)
-                        desc = [x[0] for x in cur.description]
-                        print(*desc, sep='\t')
-                        for row in cur.fetchall():
-                            for i in row :
-                                print (i, end = '\t')
-                            print()
-                            count +=1
-                            if count%30==0:
-                                reply = input ( "....more ? (y/n)")
-                                if reply != "y" :
-                                    break
-                                else:
-                                    print ()
-                    else:
-                        cur.execute(buffer)
-                        cur.execute('commit')
-                    print ("σύνολο :", cur.rowcount )
-                except pymysql.Error as e:
-                    print ("An error occurred:", e)
-                buffer = ""
+    def GiveSqlCommand (self, command):
+        # buffer = ""
+        response = {"header": "", "res": "", "error": ""}
+        print (command)
+        try:
+            command = command.strip()
+            if command.lstrip().upper().startswith("SELECT"):
+                # count=0
+                self.cur.execute(command)
+                desc = [x[0] for x in self.cur.description]
+                print(*desc, sep='\t')
+                rows = self.cur.fetchall()
+                response.update({"header": desc, "res": rows})
+                response = json.dumps(response)
+                return response
+
+            else:
+                self.cur.execute(command)
+                self.cur.execute('commit')
+                print ("σύνολο :", self.cur.rowcount )
+                response.update({"res": self.cur.rowcount})
+                response = json.dumps(response)
+                return response
+
+        except pymysql.Error as e:
+            print ("An error occurred:", e)
+            response.update({"error": "An error occurred, check again!"})
+            response = json.dumps(response)
+            return response
 
     def PrintSights (self):
         self.cur.execute("SELECT onoma FROM aksiotheata")
         # mytable=from_db_cursor(self.cur)
         # print(mytable)
-
         rows = self.cur.fetchall()
         return rows
 
     def PrintRestaurant (self):
         self.cur.execute("SELECT onoma,dieuthunsi,type FROM estiasi")
-        # mytable=from_db_cursor(self.cur)
-        # print(mytable)
         rows = self.cur.fetchall()
         return rows
 
@@ -96,6 +86,6 @@ class Database:
 
 if __name__ == '__main__':
 
-    db = Database()
+    db = Database('travel_guide')
 
 
